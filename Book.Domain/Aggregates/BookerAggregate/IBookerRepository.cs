9e3e30db7664e9d282d40753257ee8b14f41b6f0
Bug In The Air <58814071+BugInTheAir@users.noreply.github.com>
2020-12-27 12:32:26 +0700

    Remove BookApi

---
+++ Book.Domain/Aggregates/BookerAggregate/IBookerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Book.Domain.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Domain.Aggregates.BookerAggregate
+{
+    interface IBookerRepository : IRepository<Booker>
+    {
+        Booker Add(Booker booker);
+        Task<Booker> FindBookerByName(string name);
+        Task<Booker> FindBookerById(string id);
+    }
+}
